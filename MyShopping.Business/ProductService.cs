93defa46b1541c951cb08a9f4bee739efb556a75
emert117 <emremert117@gmail.com>
2021-07-03 12:05:33 +0300

    Add project files.

---
+++ MyShopping.Business/ProductService.cs
@@ -0,0 +1,26 @@
+﻿using MyShopping.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShopping.Business
+{
+    public class ProductService
+    {
+        public List<Product> GetAllProducts()
+        {
+            List<Product> allProducts = new List<Product>();
+            Product product = new Product() { ID = 1, Name = "HP", Category = "Laptop", Price = 1500m, StockQuantity = 10, Description = "iyi laptop" };
+            allProducts.Add(product);
+
+            Product product2 = new Product() { ID = 2, Name = "Asus", Category = "Gaming Laptop", Price = 1600m, StockQuantity = 15, Description = "Çalıştırmadığı oyun yok" };
+            allProducts.Add(product2);
+
+            return allProducts;
+        }
+    }
+
+   
+}
---
+++ MyShopping.Entities/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShopping.Entities
+{
+    public class Product
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public decimal Price { get; set; }
+        public int StockQuantity { get; set; }
+        public string Description { get; set; }
+    }
+}
